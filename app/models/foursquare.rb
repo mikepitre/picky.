0bc383bf21809e0cd8e6566ad9190b6331a4ba5b
Mike Pitre <michaelepitre@gmail.com>
2015-07-10 13:29:10 -0500

    made date parser in foursquare model

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,15 +1,22 @@
 class Foursquare < ActiveRecord::Base
 
-  def client_id
+  def CLIENT_ID
     ENV['foursquare_client_id']
   end
 
-  def client_secret
+  def CLIENT_SECRET
     ENV['foursquare_client_secret']
   end
 
+  def today
+    today = Date.today.to_s
+    today = today.gsub(/\W/, '')
+  end
+
+  end
+
   def get_pick
-    response = HTTParty.get('https://api.foursquare.com/v2/venues/explore?near=austin')
+    response = HTTParty.get('https://api.foursquare.com/v2/venues/explore?near=austin&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{today}')
   end
 
 
