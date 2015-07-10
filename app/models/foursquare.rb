3ad34c67b92194020be117258b928858f0d6c7c7
Mike Pitre <michaelepitre@gmail.com>
2015-07-10 12:52:25 -0500

    added httparty get request to foursquare model

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,6 +1,4 @@
 class Foursquare < ActiveRecord::Base
-  include HTTParty
-  base_uri 'api.foursquare.com/v2/venues/explore'
 
   def client_id
     ENV['foursquare_client_id']
@@ -9,6 +7,12 @@ class Foursquare < ActiveRecord::Base
   def client_secret
     ENV['foursquare_client_secret']
   end
+
+  def get_pick
+    response = HTTParty.get('https://api.foursquare.com/v2/venues/explore?near=austin')
+  end
+
+
 
 
 
