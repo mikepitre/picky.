c5fd62b0632ff3660b057d1c628bd5ed62f57a6b
Mike Pitre <michaelepitre@gmail.com>
2015-07-10 13:37:17 -0500

    removed extra end

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,5 +1,9 @@
 class Foursquare < ActiveRecord::Base
 
+# Foursquare API call URI format
+# https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
+
+
   def CLIENT_ID
     ENV['foursquare_client_id']
   end
@@ -9,10 +13,7 @@ class Foursquare < ActiveRecord::Base
   end
 
   def today
-    today = Date.today.to_s
-    today = today.gsub(/\W/, '')
-  end
-
+    today = Date.today.to_s.gsub(/\W/, '')
   end
 
   def get_pick
