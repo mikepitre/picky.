6024e143f9f775ef720ce028adbfab32981647f5
Mike Pitre <michaelepitre@gmail.com>
2015-07-10 16:16:50 -0500

    added some methods to foursquare model, fixed log in button on larger screens

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -4,22 +4,24 @@ class Foursquare < ActiveRecord::Base
 # https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
 
 
-  def CLIENT_ID
-    ENV['foursquare_client_id']
-  end
-
-  def CLIENT_SECRET
-    ENV['foursquare_client_secret']
-  end
+  CLIENT_ID = ENV['foursquare_client_id']
+  CLIENT_SECRET = ENV['foursquare_client_secret']
 
   def today
     today = Date.today.to_s.gsub(/\W/, '')
   end
 
-  def get_pick
-    response = HTTParty.get("https://api.foursquare.com/v2/venues/explore?near=austin&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{today}")
+  def get_pick(query)
+    HTTParty.get("https://api.foursquare.com/v2/venues/explore?near=austin&query=#{query}&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{today}")
   end
 
+  def pick_from_selection(selection)
+    choices = selection.attributes.map do |attribute|
+      attribute[0] if attribute[1] == true
+    end
+
+    get_pick(choices.compact.join('+'))
+  end
 
 
 end
