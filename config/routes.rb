e9b569234c10ad1d4a3525e531b3258a1cad3860
Mike Pitre <michaelepitre@gmail.com>
2015-07-10 12:01:51 -0500

    put everything besides log in and sign up behind authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   devise_for :users
-  resources :selections
-  resources :meals
 
+  authenticate :user do
+    resources :selections
+    resources :meals
+  end
 
 
 end
