6927e10c0e448a80456d6da97936b8eae3ddab4e
Mike Pitre <michaelepitre@gmail.com>
2015-07-15 17:40:30 -0500

    added methods to foursquare method, added venue info to view

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -7,21 +7,47 @@ class Foursquare < ActiveRecord::Base
   CLIENT_ID = ENV['foursquare_client_id']
   CLIENT_SECRET = ENV['foursquare_client_secret']
 
-  def today
+  def self.today
     today = Date.today.to_s.gsub(/\W/, '')
   end
 
-  def get_pick(query)
+  def self.get_pick(query)
     HTTParty.get("https://api.foursquare.com/v2/venues/explore?near=austin&openNow=1&query=#{query}&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{today}")
   end
 
-  def pick_from_selection(selection)
+  def self.pick_from_selection(selection)
     choices = selection.attributes.map do |attribute|
       attribute[0] if attribute[1] == true
     end
 
     get_pick(choices.compact.join('+'))
   end
+
+  def self.venue_info
+    Foursquare.pick_from_selection(Selection.last)['response']['groups'].first['items']
+  end
+
+  def self.pick_name
+    venue_info.map { |x| x['venue']['name'] }.uniq.first
+  end
+
+  def self.pick_address
+    venue_info.map { |x| x['venue']['location']['address']}.first
+  end
+
+  def self.pick_city_and_zip
+    venue_info.map { |x| x['venue']['location']['formattedAddress']}.first[1]
+  end
+
+  def self.pick_phone_number
+    venue_info.map { |x| x['venue']['contact']['formattedPhone']}.first
+  end
+
+  def self.pick_website
+    venue_info.map { |x| x['venue']['url']}.first
+  end
+
+
 
 
 end
