6e877f4408145d4192e70cc747e2cf5b7e099dd4
Mike Pitre <michaelepitre@gmail.com>
2015-07-17 00:18:19 -0500

    fixed issue with & in venue names

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -34,7 +34,7 @@ class Foursquare < ActiveRecord::Base
       choices += ["&radius=#{selection.distance.to_f/0.00062137}"]
     end
 
-    get_pick(choices.compact.join("+").gsub(/serves_alcohol/,"drinks").gsub(/outdoor_seating/,"outdoor+seating").gsub(/take_out/, "take+out"))
+    get_pick(choices.compact.join("+").gsub(/serves_alcohol/,"drinks+beer").gsub(/outdoor_seating/,"outdoor+seating").gsub(/take_out/, "take+out"))
   end
 
   def response
