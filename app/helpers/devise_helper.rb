71b63d760652ca701cfeafb507052fb18d0b460d
Mike Pitre <michaelepitre@gmail.com>
2015-07-18 20:24:19 -0500

    styled devise error messages

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+module DeviseHelper
+ def devise_error_messages!
+  return '' if resource.errors.empty?
+
+   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+   sentence = I18n.t('errors.messages.not_saved',
+   count: resource.errors.count,
+   resource: resource.class.model_name.human.downcase)
+
+   html = <<-HTML
+   <div class="alert alert-error centered error-messages">
+      <button type="button" class="btn" data-dismiss="alert">#{messages}</button>
+   </div>
+   HTML
+
+   html.html_safe
+ end
+end
